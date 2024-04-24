9460d2e26d80bdc7d734be000b8de9957b1ae485
nde95 <ndennison268@gmail.com>
2024-04-24 12:52:26 -0400

    initial commit

---
+++ books-network-api/src/main/java/com/books/api/BooksNetworkApiApplication.java
@@ -0,0 +1,13 @@
+package com.books.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksNetworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksNetworkApiApplication.class, args);
+	}
+
+}
