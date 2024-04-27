395e2c79f9a2c53559cea10ac8a20b164d4193d6
nde95 <ndennison268@gmail.com>
2024-04-27 13:07:55 -0400

    User class and JPA auditing

--- books-network-api/src/main/java/com/books/api/BooksNetworkApiApplication.java
+++ books-network-api/src/main/java/com/books/api/BooksNetworkApiApplication.java
@@ -2,8 +2,10 @@ package com.books.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BooksNetworkApiApplication {
 
 	public static void main(String[] args) {
